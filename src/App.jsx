35570c0226e33c24bb2462980c24a7b571b0d6de
dcence-raya <dcenceraya@gmail.com>
2026-05-04 11:38:54 +0545

    Fix case-sensitivity in imports for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
 import { AppProvider } from "./contexts/AppContext";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Experience from "./components/Experience";
+import Hero from "./components/hero";
+import Experience from "./components/experience";
 import Menu from "./components/Menu";
 import Reservation from "./components/Reservation";
 import Footer from "./components/footers";
